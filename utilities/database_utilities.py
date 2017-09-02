99d2906386fb0bbe368eed3347045c663b623512
ipev <ipe.m.varghese@gmail.com>
2017-09-02 12:38:41 +1000

    intial commit

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -3,5 +3,5 @@ def create_database(database_path:str):
     pass
 
 def save_words_to_database(database_path:str,words_list:list):
-    #TODO: save the words to database
+    #TODO: save the words to database with Ipe
     pass
